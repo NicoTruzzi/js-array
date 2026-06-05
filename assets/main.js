14d9d2ff531077238f183277d3dd742e954bef2d
Nicolò <nicolo.truzzi1@gmail.com>
2026-06-05 21:20:29 +0200

    esercizio 28

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,28 @@
 console.log("script caricato");
 
-const azioni = ["elemento 1", "elemento 2", "elemento 3", "elemento 4"];
-console.log(azioni);
+const menu = ["piatto 1", "piatto 2", "piatto 3", "piatto 4"];
+const allergeni = [
+    ["arachidi", "miele"],
+    ["uova", "senape", "pesce"],
+    ["crostacei"],
+    ["grano", "sedano", "nocciole", "pistacchi"]
+];
+console.log(menu);
+console.log(allergeni);
 console.log("");
+let piatto = prompt("scegli un piatto");
 
-azioni.reverse();
-azioni.shift();
-console.log("nuova lista di azioni", azioni);
+let allergene = prompt("scegli un allergene");
+for (let i = 0; i < menu.length; i++) {
+    if (piatto === menu[i]) {
+        for (let j = 0; j < allergeni[i].length; j++) {
+            if (allergeni[j].includes(allergene)) {
+                console.log(allergene, "è nella lista degli allergeni del piatto scelto");
+
+            }
+        }
+
+    }
+}
 
 
--- pseudocodice.md
+++ pseudocodice.md
@@ -1,3 +1,3 @@
 # ESERCIZIO
 
-27. Stai creando una funzione "Undo" (Annulla); inverti l'ordine delle azioni compiute per poterle annullare nell'ordine corretto.
+28. In un menu digitale, verifica se un ingrediente specifico è presente nella lista degli allergeni di un piatto.
