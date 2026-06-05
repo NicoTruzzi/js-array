1aff43a14c655001f7ce11ce7583ecb9914ad47a
Nicolò <nicolo.truzzi1@gmail.com>
2026-06-05 16:10:00 +0200

    esercizio 6

--- assets/main.js
+++ assets/main.js
@@ -1,39 +1,15 @@
 console.log("script caricato");
 
-const vip = ["Alessandro", "Giulia", "Lorenzo", "Sofia", "Matteo", "Aurora", "Francesco", "Ginevra", "Leonardo", "Martina", "Tommaso", "Chiara", "Riccardo", "Alice", "Davide"];
-const standard = ["Emma", "Federico", "Beatrice", "Andrea", "Vittoria", "Marco", "Greta", "Gabriele", "Elena", "Simone", "Gaia", "Niccolò", "Noemi", "Filippo", "Camilla"];
-let presente = false;
-console.log("senza funzione js");
-console.log("");
-const nome = prompt("quale è il tuo nome?");
-for (let i = 0; i < vip.length; i++) {
-    if (nome === vip[i]) {
-        console.log("sei nella lista VIP", nome);
-        presente = true;
-        break;
+const OreLav = [40, 35, 45, 38, 42];
+
+for (let i = 0; i < OreLav.length; i++) {
+    let = paga = 0;
+    if (OreLav[i] > 40) {
+        paga = (15 * 40) + (20 * (OreLav[i] - 40))
+        console.log("la paga di", i, "è", paga);
+    } else {
+        paga = 15 * OreLav[i];
+        console.log("la paga di", i, "è", paga);
+
     }
 }
-for (let i = 0; i < standard.length; i++) {
-    if (nome === standard[i]) {
-        console.log("sei nella lista STANDARD", nome);
-        presente = true;
-        break;
-    }
-}
-if (presente === false) {
-    console.log("non sei presente nelle liste vip/standard");
-}
-
-console.log("");
-console.log("con funzioni js");
-console.log("");
-if (vip.includes(nome)) {
-    console.log("sei nella lista VIP", nome);
-
-} else if (standard.includes(nome)) {
-    console.log("sei nella lista STANDARD", nome);
-} else {
-    console.log("non sei presente nelle liste vip/standard", nome);
-
-}
-
--- pseudocodice.md
+++ pseudocodice.md
@@ -1,3 +1,3 @@
 # ESERCIZIO
 
-5. Data la lista VIP per una serata, verifica se l'utente X è presente nella lista VIP o se è nella lista standard.
+6. Date le ore lavorative giornaliere [40, 35, 45, 38, 42] e una paga base di 15€/ora, calcola lo stipendio totale settimanale applicando una maggiorazione di 5€ per ogni ora straordinaria oltre le 40 giornaliere.
